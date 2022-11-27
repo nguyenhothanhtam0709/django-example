2a4c51f652ebf5b3ac9299a52bedf0c6d41813d8
TamNHT <nguyenhothanhtam0709@gmail.com>
2022-11-27 19:35:32 +0700

    add user-blog relation

--- simple_blog/serializers/posts.py
+++ simple_blog/serializers/posts.py
@@ -1,6 +1,7 @@
 from django.db.models import fields
 from rest_framework import serializers
 from simple_blog.models import Post
+from .user import UserSerializer
 
 
 class CreatePostSerializer (serializers.ModelSerializer):
@@ -8,7 +9,19 @@ class CreatePostSerializer (serializers.ModelSerializer):
         model = Post
         fields = ('title', 'content')
 
-        
+    def create(self, validated_data):
+        user = self.context.get('user', None)
+        return Post.objects.create(user=user, **validated_data)
+
+
+class RetrievePostSerializer (serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
 class PostSerializer (serializers.ModelSerializer):
     class Meta:
         model = Post
